05857b9a6e06f0f6952e26ea995a85614204e433
Ivan Sapozhnik <sapozhnik.ivan@gmail.com>
2019-03-09 08:03:01 +0100

    Initial Commit

---
+++ MVGBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MVGBar
+//
+//  Created by Ivan Sapozhnik on 3/9/19.
+//  Copyright © 2019 Ivan Sapozhnik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
